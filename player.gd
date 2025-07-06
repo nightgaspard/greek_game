a8ee2734aeb6106fd22349160ed6c3a246773ded
Night Gaspard <nightgaspard@proton.me>
2025-07-06 03:16:06 -0500

    Adds background

--- player.gd
+++ player.gd
@@ -3,7 +3,6 @@ class_name Player extends RigidBody2D
 
 @export var default_roll_force:float = 500
 @export var default_jump_force:float  = 1500
-@onready var boulder = $"../Boulder"
 
 @onready var gruntPlayer:AudioStreamPlayer = $GruntPlayer
 var grunt_playing: bool = false
@@ -13,6 +12,7 @@ var grunt_playing: bool = false
 @onready var debug_ray_cast_right: Line2D = $DebugRayCastRight
 @onready var debug_ray_cast_left: Line2D = $DebugRayCastLeft
 @onready var player_sprite: Sprite2D = $PlayerSprite
+var last_direction: String = ""
 
 var grunt_sounds: Array[AudioStreamWAV] = [
 	preload("res://grunts/grunting_1.wav"),
@@ -54,14 +54,17 @@ func _process(_delta:float) -> void:
 	
 	if Input.is_action_pressed("right"):
 		self.apply_central_force(Vector2.RIGHT*roll_force * _delta)
+		last_direction = "right"
 	if Input.is_action_pressed("left"):
 		self.apply_central_force(Vector2.LEFT*roll_force * _delta)
+		last_direction = "left"
 	if Input.is_action_just_pressed("jump"):
 		self.apply_central_force(Vector2.UP*jump_force * _delta)
 	
-	if self.linear_velocity.x > 0:
+	if self.linear_velocity.x > 60.0 and last_direction == "right":
 		player_sprite.flip_h = false
-	elif  self.linear_velocity.x < 0:
+		
+	elif  self.linear_velocity.x < -60.0 and last_direction == "left":
 		player_sprite.flip_h = true
 	
 	var should_play_grunt: bool = gruntPlayer and grunt_playing == false
@@ -76,6 +79,8 @@ func _process(_delta:float) -> void:
 
 func _on_audio_stream_player_finished() -> void:
 	gruntPlayer.stream = grunt_sounds[randi() % grunt_sounds.size()]
+	var timer = get_tree().create_timer(1.0)
+	await timer.timeout
 	grunt_playing = false
 	pass # Replace with function body.
 
